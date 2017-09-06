ec124b3e6bda9c3544747d73506c10a1b550bb72
George Schneeloch <gschneel@mit.edu>
2017-09-06 14:07:43 -0400

    Added wait_for_deploy.py

--- wait_for_deploy.sh
+++ wait_for_deploy.sh
@@ -4,38 +4,6 @@
 # SCRIPT_DIR via http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-. $SCRIPT_DIR/release.sh
-
-
-compare_hash() {
-    git checkout "$WATCH_BRANCH"
-    CURRENT_HASH=$(git rev-parse $WATCH_BRANCH)
-    [[ "$CURRENT_HASH" == "$RELEASE_HASH" ]]
-}
-
-fetch_release_hash() {
-    RELEASE_HASH=$(curl "$HASH_URL")
-    if [[ ${#RELEASE_HASH} -ne 40 ]];
-    then
-        error "Error: expected release hash from $HASH_URL"
-        error "but got: $RELEASE_HASH"
-        exit 1
-    fi
-}
-
-main() {
-    fetch_release_hash
-    validate_dependencies
-    create_working_dir
-    while ! compare_hash
-    do
-        sleep 30
-        fetch_release_hash
-    done
-    echo "Hashes match, deployment was successful"
-    delete_working_dir
-}
-
 if [[ $(basename $0) = "wait_for_deploy.sh" ]]; then
     set -euf -o pipefail
 
@@ -49,25 +17,24 @@ if [[ $(basename $0) = "wait_for_deploy.sh" ]]; then
     WATCH_BRANCH="${3:-}"
 
     if [[ -z "$REPO_DIR" ]]; then
-        error "You must specify your git repo directory as the first argument."
+        echo "You must specify your git repo directory as the first argument."
         exit 1
     fi
 
     if [[ "$HASH_URL" != https* ]]; then
-        error "You must specify a hash URL to compare with the release candidate."
-        error "For example, for micromasters https://micromasters-rc.herokuapp.com/static/hash.txt"
+        echo "You must specify a hash URL to compare with the release candidate."
+        echo "For example, for micromasters https://micromasters-rc.herokuapp.com/static/hash.txt"
         exit 1
     fi
 
     if [[ -z "$WATCH_BRANCH" ]]
     then
-        error "You must specify a branch whose latest commit will match the deployed hash"
-        error "for example release-candidate or release"
+        echo "You must specify a branch whose latest commit will match the deployed hash"
+        echo "for example release-candidate or release"
         exit 1
     fi
 
-    # make into absolute path
-    REPO_DIR="$(cd "$REPO_DIR"; pwd)"
+    REPO_URL=$(git --git-dir "$REPO_DIR"/.git remote get-url origin)
 
-    main
+    python3 "$SCRIPT_DIR"/wait_for_deploy.py "$REPO_URL" "$HASH_URL" "$WATCH_BRANCH"
 fi
