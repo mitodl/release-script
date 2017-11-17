f031d99f3f9c5ea8b48c08511c0218c93b27a714
George Schneeloch <gschneel@mit.edu>
2017-11-17 12:28:37 -0500

    Replace hub with API call to create a PR, add package.json to install git-release-notes

---
+++ repo_info.py
@@ -0,0 +1,10 @@
+"""Information about repositories"""
+from collections import namedtuple
+
+RepoInfo = namedtuple('RepoInfo', [
+    'name',
+    'repo_url',
+    'rc_hash_url',
+    'prod_hash_url',
+    'channel_id',
+])
