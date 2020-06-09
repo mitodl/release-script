b4184723169e0cf07bc21dd4f0103583d3bec694
initial <initial@example.com>
2020-06-09 12:34:00 -0400

    Migrate to slack conversations API

--- slack.py
+++ slack.py
@@ -14,19 +14,12 @@ async def get_channels_info(slack_access_token):
     """
     client = ClientWrapper()
     # public channels
-    resp = await client.post("https://slack.com/api/channels.list", data={
-        "token": slack_access_token
+    resp = await client.post("https://slack.com/api/conversations.list", data={
+        "token": slack_access_token,
+        "types": "public_channel,private_channel"
     })
     resp.raise_for_status()
     channels = resp.json()['channels']
     channels_map = {channel['name']: channel['id'] for channel in channels}
 
-    # private channels
-    resp = await client.post("https://slack.com/api/groups.list", data={
-        "token": slack_access_token
-    })
-    resp.raise_for_status()
-    groups = resp.json()['groups']
-    groups_map = {group['name']: group['id'] for group in groups}
-
-    return {**channels_map, **groups_map}
+    return channels_map
--- slack_test.py
+++ slack_test.py
@@ -12,20 +12,29 @@ async def test_get_channels_info(mocker):
     """get_channels_info should obtain information for all public and private channels that doof knows about"""
     post_patch = mocker.async_patch('client_wrapper.ClientWrapper.post')
     post_patch.return_value.json.side_effect = [
-        {'channels': [{
-            'name': 'a',
-            'id': 'public channel',
-        }]},
-        {'groups': [{
-            'name': 'and a',
-            'id': 'private one',
-        }]},
+        {
+            'channels': [
+                {
+                    'name': 'a',
+                    'id': 'public channel',
+                },
+                {
+                    'name': 'and a',
+                    'id': 'private one',
+                },
+            ]
+        },
     ]
     token = 'token'
     assert await get_channels_info(token) == {
         'a': 'public channel',
         'and a': 'private one',
     }
-    post_patch.assert_any_call(mocker.ANY, "https://slack.com/api/channels.list", data={'token': token})
-    post_patch.assert_any_call(mocker.ANY, "https://slack.com/api/groups.list", data={'token': token})
-    assert post_patch.return_value.raise_for_status.call_count == 2
+    post_patch.assert_called_once_with(
+        mocker.ANY,
+        "https://slack.com/api/conversations.list",
+        data={
+            'token': token,
+            "types": "public_channel,private_channel",
+        },
+    )
