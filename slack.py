d683de2043cd7b038174100c7e90e336d03afeec
initial <initial@example.com>
2020-06-09 13:00:26 -0400

    Pagination

--- slack.py
+++ slack.py
@@ -14,12 +14,21 @@ async def get_channels_info(slack_access_token):
     """
     client = ClientWrapper()
     # public channels
-    resp = await client.post("https://slack.com/api/conversations.list", data={
-        "token": slack_access_token,
-        "types": "public_channel,private_channel"
-    })
-    resp.raise_for_status()
-    channels = resp.json()['channels']
-    channels_map = {channel['name']: channel['id'] for channel in channels}
+    next_cursor = None
+    channels = []
 
-    return channels_map
+    while True:
+        resp = await client.post("https://slack.com/api/conversations.list", data={
+            "token": slack_access_token,
+            "types": "public_channel,private_channel",
+            **({"cursor": next_cursor} if next_cursor is not None else {})
+        })
+        resp.raise_for_status()
+        resp_json = resp.json()
+        channels.extend(resp_json['channels'])
+
+        next_cursor = resp_json.get("response_metadata", {}).get("next_cursor")
+        if not next_cursor:
+            break
+
+    return {channel['name']: channel['id'] for channel in channels}
--- slack_test.py
+++ slack_test.py
@@ -11,6 +11,7 @@ pytestmark = pytest.mark.asyncio
 async def test_get_channels_info(mocker):
     """get_channels_info should obtain information for all public and private channels that doof knows about"""
     post_patch = mocker.async_patch('client_wrapper.ClientWrapper.post')
+    next_cursor = 'some cursor'
     post_patch.return_value.json.side_effect = [
         {
             'channels': [
@@ -22,6 +23,17 @@ async def test_get_channels_info(mocker):
                     'name': 'and a',
                     'id': 'private one',
                 },
+            ],
+            'response_metadata': {
+                'next_cursor': next_cursor,
+            }
+        },
+        {
+            'channels': [
+                {
+                    'name': 'two',
+                    'id': 'a channel in the next page'
+                }
             ]
         },
     ]
@@ -29,12 +41,22 @@ async def test_get_channels_info(mocker):
     assert await get_channels_info(token) == {
         'a': 'public channel',
         'and a': 'private one',
+        'two': 'a channel in the next page',
     }
-    post_patch.assert_called_once_with(
+    post_patch.assert_any_call(
         mocker.ANY,
         "https://slack.com/api/conversations.list",
         data={
             'token': token,
             "types": "public_channel,private_channel",
+        },
+    )
+    post_patch.assert_any_call(
+        mocker.ANY,
+        "https://slack.com/api/conversations.list",
+        data={
+            'token': token,
+            "types": "public_channel,private_channel",
+            "cursor": next_cursor,
         },
     )
