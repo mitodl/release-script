ad94ac27d226ccc745b2517e2b8d7fd42271f3c4
George Schneeloch <gschneel@mit.edu>
2021-06-02 16:21:27 -0400

    Retry on GET failures and connection errors

--- client_wrapper.py
+++ client_wrapper.py
@@ -1,18 +1,26 @@
 """Wrapper for HTTP client. Replaces httpx until it matures."""
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 
 class ClientWrapper:
     """Wrapper for requests or httpx to make an HTTP request"""
 
+    def __init__(self):
+        self.session = requests.Session()
+        adapter = HTTPAdapter(max_retries=Retry())
+        self.session.mount("http://", adapter)
+        self.session.mount("https://", adapter)
+
     async def get(self, *args, **kwargs):
         """GET request"""
-        return requests.get(*args, **kwargs)
+        return self.session.get(*args, **kwargs)
 
     async def post(self, *args, **kwargs):
         """POST request"""
-        return requests.post(*args, **kwargs)
+        return self.session.post(*args, **kwargs)
 
     async def delete(self, *args, **kwargs):
         """DELETE request"""
-        return requests.delete(*args, **kwargs)
+        return self.session.delete(*args, **kwargs)
